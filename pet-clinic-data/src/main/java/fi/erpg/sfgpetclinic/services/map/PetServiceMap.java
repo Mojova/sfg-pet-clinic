d38fe705ee09d1a29c7a5c123aa4698f3264a21f
Jaakko Kaisanlahti <jaakko.kaisanlahti@erpg.fi>
2019-11-09 15:51:48 +0200

    Impl map based services. Closes #6

---
+++ pet-clinic-data/src/main/java/fi/erpg/sfgpetclinic/services/map/PetServiceMap.java
@@ -0,0 +1,33 @@
+package fi.erpg.sfgpetclinic.services.map;
+
+import fi.erpg.sfgpetclinic.model.Pet;
+import fi.erpg.sfgpetclinic.services.CrudService;
+
+import java.util.Set;
+
+public class PetServiceMap extends AbstractMapService<Pet, Long> implements CrudService<Pet, Long> {
+    @Override
+    public Set<Pet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Pet findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Pet save(Pet object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public void delete(Pet object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
