0896252a638b1825d3bb5e8af721da0338ca08bd
Jaakko Kaisanlahti <jaakko.kaisanlahti@erpg.fi>
2019-12-14 16:53:17 +0200

    Added Pets and contact info. Closes #33.

--- pet-clinic-web/src/main/java/fi/erpg/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/fi/erpg/sfgpetclinic/bootstrap/DataLoader.java
@@ -1,13 +1,17 @@
 package fi.erpg.sfgpetclinic.bootstrap;
 
 import fi.erpg.sfgpetclinic.model.Owner;
+import fi.erpg.sfgpetclinic.model.Pet;
 import fi.erpg.sfgpetclinic.model.PetType;
 import fi.erpg.sfgpetclinic.model.Vet;
 import fi.erpg.sfgpetclinic.services.OwnerService;
 import fi.erpg.sfgpetclinic.services.PetTypeService;
 import fi.erpg.sfgpetclinic.services.VetService;
+import org.apache.tomcat.jni.Local;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
 
 @Component
 public class DataLoader implements CommandLineRunner {
@@ -36,12 +40,33 @@ public class DataLoader implements CommandLineRunner {
         Owner owner1 = new Owner();
         owner1.setFirstName("Olli");
         owner1.setLastName("Omisaja");
+        owner1.setAddress("Kaduntie 1");
+        owner1.setCity("Kaupunkila");
+        owner1.setTelephone("040 123 1234");
+
+        Pet ollisPet = new Pet();
+        ollisPet.setPetType(savedDogPetType);
+        ollisPet.setOwner(owner1);
+        ollisPet.setBirthDate(LocalDate.now());
+        ollisPet.setName("Hunde");
+        owner1.getPets().add(ollisPet);
+
 
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
         owner2.setFirstName("Outi");
         owner2.setLastName("Omistaja");
+        owner2.setAddress("Tienkatu 1");
+        owner2.setCity("Kylälä");
+        owner2.setTelephone("040 431 5431");
+
+        Pet outisPet = new Pet();
+        outisPet.setPetType(savedCatPetType);
+        outisPet.setName("Monni");
+        outisPet.setBirthDate(LocalDate.now());
+        outisPet.setOwner(owner2);
+        owner2.getPets().add(outisPet);
 
         ownerService.save(owner2);
 
